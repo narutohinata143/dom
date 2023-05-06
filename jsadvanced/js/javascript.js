cca99ee5fa9d997942ad4344d1f418d9c69f46f6
murugesan vijaya <murugesanvijaya160@gmail.com>
2023-05-06 08:02:01 +0000

    getelemnt byid topic

---
+++ jsadvanced/js/javascript.js
@@ -0,0 +1,8 @@
+let user  = document.getElementById("user");
+let button = document.getElementById("button");
+let response = document.getElementById("response");
+
+button.addEventListener("click" , ()=>
+{
+console.log(response.innerText  = user.value) ;
+})
