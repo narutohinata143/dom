19cadaee96009222159c1843913b6ce66ec7cfae
murugesan vijaya <murugesanvijaya160@gmail.com>
2023-05-06 09:02:41 +0000

    getelemntbyclassname topic

--- jsadvanced/js/javascript.js
+++ jsadvanced/js/javascript.js
@@ -1,8 +1,19 @@
 let user  = document.getElementById("user");
 let button = document.getElementById("button");
-let response = document.getElementById("response");
+// let response = document.getElementById("response");
+
+// button.addEventListener("click" , ()=>
+// {
+// console.log(response.innerText  = user.value) ;
+// })
+
+let added = document.getElementById("food");
 
 button.addEventListener("click" , ()=>
 {
-console.log(response.innerText  = user.value) ;
+    added.innerHTML += `<li class="food-item">${user.value.toUpperCase()}</li>`;
 })
+
+let getting = document.getElementsByClassName("food-item veg");
+
+console.log(getting[0].innerHTML = "sadni");
