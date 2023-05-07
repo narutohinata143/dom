56ef7d0a311f381b8f1b6220f555e371e878cd4c
murugesan vijaya <murugesanvijaya160@gmail.com>
2023-05-07 10:29:16 +0000

    fragment

--- jsadvanced/js/javascript.js
+++ jsadvanced/js/javascript.js
@@ -1,28 +1,28 @@
-// let user  = document.getElementById("user");
-// let button = document.getElementById("button");
-// let response = document.getElementById("response");
+// // let user  = document.getElementById("user");
+// // let button = document.getElementById("button");
+// // let response = document.getElementById("response");
 
-// button.addEventListener("click" , ()=>
-// {
-// console.log(response.innerText  = user.value) ;
-// })
+// // button.addEventListener("click" , ()=>
+// // {
+// // console.log(response.innerText  = user.value) ;
+// // })
 
-// let added = document.getElementById("food");
+// // let added = document.getElementById("food");
 
-// button.addEventListener("click" , ()=>
-// {
-//     added.innerHTML += `<li class="food-item">${user.value.toUpperCase()}</li>`;
-// })
+// // button.addEventListener("click" , ()=>
+// // {
+// //     added.innerHTML += `<li class="food-item">${user.value.toUpperCase()}</li>`;
+// // })
 
-// let getting = document.getElementsByClassName("food-item veg");
+// // let getting = document.getElementsByClassName("food-item veg");
 
-// console.log(getting[0].innerHTML = "sadni");
+// // console.log(getting[0].innerHTML = "sadni");
 
-// let santhosh = document.getElementsByTagName('li');
-// console.log(santhosh);
+// // let santhosh = document.getElementsByTagName('li');
+// // console.log(santhosh);
 
-// let quer = document.querySelectorAll("div"); // ethu vanthu html array va tharama html node aha value tharum thats call qery selector
-// console.log(quer[1]);
+// // let quer = document.querySelectorAll("div"); // ethu vanthu html array va tharama html node aha value tharum thats call qery selector
+// // console.log(quer[1]);
 
 let inputFood = document.getElementById("input-food");
 let inputBtn = document.getElementById("input-btn");
@@ -43,3 +43,45 @@ inputBtn.addEventListener("click", () => {
 
   foodContainer.append(li);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- jsadvanced/js/main.js
+++ jsadvanced/js/main.js
@@ -1,27 +1,40 @@
-// let main = document.getElementById("main");
+// // let main = document.getElementById("main");
 
-// let div = document.createElement("div");
+// // let div = document.createElement("div");
 
-// let add =  document.createTextNode("THIS ADD SOME KIND OF MESSAGE");
-// div.append(add);
-// console.log(div , add);
+// // let add =  document.createTextNode("THIS ADD SOME KIND OF MESSAGE");
+// // div.append(add);
+// // console.log(div , add);
 
-// main.append(div);
-// console.log(main);
+// // main.append(div);
+// // console.log(main);
 
-function createAlertViaDOM(message) {
-    const main = document.getElementById("main");
+// function createAlertViaDOM(message) {
+//     const main = document.getElementById("main");
   
-    // Creating div element
-    const div = document.createElement("div");
-    const textNode = document.createTextNode(message);
+//     // Creating div element
+//     const div = document.createElement("div");
+//     const textNode = document.createTextNode(message);
   
-    // Assigning className
-    div.className = "alert";
+//     // Assigning className
+//     div.className = "alert";
   
-    // Append
-    div.append(textNode);
-    main.append(div);
-  }
+//     // Append
+//     div.append(textNode);
+//     main.append(div);
+//   }
   
-  createAlertViaDOM("Food is updated");
+//   createAlertViaDOM("Food is updated");
+
+let main = document.getElementById("food-container")
+console.log(main);
+let fragment = document.createDocumentFragment();
+for(let i=1; i<=1000 ; i++)
+{
+  let li =  document.createElement('li');
+  li.textContent = `i love you ${i}`
+  li.className =  "food-item";
+  fragment.append(li);
+}
+
+main.append(fragment);
